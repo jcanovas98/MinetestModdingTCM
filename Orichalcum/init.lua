3b1556823442dc03f40bbda7051ba52fb82310f0
jcanovas <jcanovas@edu.tecnocampus.cat>
2020-11-23 17:14:39 +0100

    Oricahlculism v1

--- Orichalcum/init.lua
+++ Orichalcum/init.lua
@@ -1,92 +1,163 @@
+-- ORICHALCUM STONE
 minetest.register_node("orichalcum:stone_with_orichalcum", {
-description = "My node",
-tiles = {"mymod_mynode.png"},
+description = "Orichalcum",
+tiles = {"ori_stone.png"},
 groups = {cracky=3}
+drop = "orichalcum:orichalcum_lump"
 })
 
+-- ORICHALCUM LUMP
 minetest.register_craftitem("orichalcum:orichalcum_lump", {
-description = "My Item",
-inventory_image = "mymod_myitem.png"
+description = "Orichalcum Lump",
+inventory_image = "ori_lump.png"
 })
 
+-- ORICHALCUM INGOT
 minetest.register_craftitem("orichalcum:orichalcum_ingot", {
-description = "My Item",
-inventory_image = "mymod_myitem.png"
+description = "Orichalcum Ingot",
+inventory_image = "ori_ingot.png"
+})
+
+
+
+-- ORE GEN
+minetest.register_ore({
+  ore_type       = "scatter",
+  ore            = "orichalcum:stone_with_orichalcum",
+  wherein        = "default:stone",
+  clust_scarcity = 8 * 8 * 8,
+  clust_num_ores = 9,
+  clust_size     = 3,
+  y_max          = 31000,
+  y_min          = 1025,
+})
+
+
+--COOKING
+minetest.register_craft({
+    type = "cooking",
+    output = "orichalcum:orichalcum_ingot",
+    recipe = "orichalcum:orichalcum_lump",
+    cooktime = 10,
+})
+
+
+-- SWORD
+minetest.register_tool(" orichalcum:sword_orichalcum ", {
+description = "Orichalcum Sword",
+inventory_image = "ori_sword.png",
+tool_capabilities = {
+full_punch_interval = 0.5,
+max_drop_level = 1,
+groupcaps = {
+crumbly = {
+maxlevel = 2,
+uses = 200,
+times = { [1]=1.60, [2]=1.20, [3]=0.80 }
+},
+},
+damage_groups = {fleshy=10},
+},
 })
 
 minetest.register_craft({
-output = "mymod:mycraft",
+output = "orichalcum:sword_orichalcum",
 recipe = {
-{"mymod:myitem", "" },
-{"mymod:myitem","mymod:myitem"},
-{"mymod:myitem","mymod:myitem"}
+{"", "orichalcum:orichalcum_ingot", ""},
+{"", "orichalcum:orichalcum_ingot", ""},
+{"", "default:stick", ""}
 }
 })
 
-minetest.register_tool(" orichalcum:sword_orichalcum ", {
-description = "My Tool",
-inventory_image = "mymod_mytool.png",
+
+
+
+-- PICKAXE
+minetest.register_tool(" orichalcum:pickaxe_orichalcum ", {
+description = "Orichalcum Pickaxe",
+inventory_image = "ori_pickaxe.png",
 tool_capabilities = {
-full_punch_interval = 1.5,
+full_punch_interval = 0.5,
 max_drop_level = 1,
 groupcaps = {
 crumbly = {
 maxlevel = 2,
-uses = 20,
+uses = 200,
 times = { [1]=1.60, [2]=1.20, [3]=0.80 }
 },
 },
-damage_groups = {fleshy=2},
+damage_groups = {cracky=10},
 },
 })
 
-minetest.register_tool("orichalcum:pick_orichalcum", {
-description = "My Tool",
-inventory_image = "mymod_mytool.png",
+minetest.register_craft({
+output = "orichalcum:pickaxe_orichalcum",
+recipe = {
+{"orichalcum:orichalcum_ingot", "orichalcum:orichalcum_ingot", "orichalcum:orichalcum_ingot"},
+{"", "default:stick", ""},
+{"", "default:stick", ""}
+}
+})
+
+
+
+
+-- AXE
+minetest.register_tool(" orichalcum:axe_orichalcum ", {
+description = "Orichalcum Axe",
+inventory_image = "ori_axe.png",
 tool_capabilities = {
-full_punch_interval = 1.5,
+full_punch_interval = 0.5,
 max_drop_level = 1,
 groupcaps = {
 crumbly = {
 maxlevel = 2,
-uses = 20,
+uses = 200,
 times = { [1]=1.60, [2]=1.20, [3]=0.80 }
 },
 },
-damage_groups = {fleshy=2},
+damage_groups = {choppy=10},
 },
 })
 
-minetest.register_tool("orichalcum:shovel_orichalcum", {
-description = "My Tool",
-inventory_image = "mymod_mytool.png",
+minetest.register_craft({
+output = "orichalcum:axe_orichalcum",
+recipe = {
+{"orichalcum:orichalcum_ingot", "orichalcum:orichalcum_ingot", ""},
+{"orichalcum:orichalcum_ingot", "default:stick", ""},
+{"", "default:stick", ""}
+}
+})
+
+
+
+
+-- SHOVEL
+minetest.register_tool(" orichalcum:shovel_orichalcum ", {
+description = "Orichalcum Shovel",
+inventory_image = "ori_shovel.png",
 tool_capabilities = {
-full_punch_interval = 1.5,
+full_punch_interval = 0.5,
 max_drop_level = 1,
 groupcaps = {
 crumbly = {
 maxlevel = 2,
-uses = 20,
+uses = 200,
 times = { [1]=1.60, [2]=1.20, [3]=0.80 }
 },
 },
-damage_groups = {fleshy=2},
+damage_groups = {crumbly=10},
 },
 })
 
-minetest.register_tool("orichalcum:axe_orichalcum", {
-description = "My Tool",
-inventory_image = "mymod_mytool.png",
-tool_capabilities = {
-full_punch_interval = 1.5,
-max_drop_level = 1,
-groupcaps = {
-crumbly = {
-maxlevel = 2,
-uses = 20,
-times = { [1]=1.60, [2]=1.20, [3]=0.80 }
-},
-},
-damage_groups = {fleshy=2},
-},
+minetest.register_craft({
+output = "orichalcum:shovel_orichalcum",
+recipe = {
+{"", "orichalcum:orichalcum_ingot", ""},
+{"", "default:stick", ""},
+{"", "default:stick", ""}
+}
 })
+
+
+
