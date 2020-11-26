b4e11007c8577886717253d179e57fdf2eb5382f
Pablo <pablo.scagliotti@gmail.com>
2020-11-26 18:25:12 +0100

    rotModd 0.2

---
+++ rotMode/init.lua
@@ -0,0 +1,7 @@
+-- ROT STONE
+minetest.register_node("rotMode:dirt_rot", {
+description = "rot",
+tiles = {"dirt_rot.png"},
+groups = {snappy = 1, bendy = 2, cracky = 1, melty = 2, level = 2},
+sounds = default_metal_sounds,
+})
