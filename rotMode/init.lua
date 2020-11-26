e6f240464ae8cf1fbf2b19a07a04658bfd2b0142
jcanovas <jcanovas@edu.tecnocampus.cat>
2020-11-26 20:54:29 +0100

    rotmod v1.0 Tema podrido funciona bien, falta tema cura

--- rotMode/init.lua
+++ rotMode/init.lua
@@ -1,31 +1,53 @@
 -- ROT STONE
-minetest.register_node("rotMode:dirt_rot", {
+minetest.register_node("rotmod:dirt_rot", {
 description = "rot",
-tiles = {"rot_up.png",    -- y+
+tiles = {"rot_up.png",   -- y+
         "rot_down.png",  -- y-
-        "rot_side.png", -- x+
+        "rot_side.png",  -- x+
         "rot_side.png",  -- x-
         "rot_side.png",  -- z+
-        "rot_side.png",}, --z-
+        "rot_side.png",  -- z-
+      }, 
 is_ground_content = true,
 groups = {crumbly=3}
 })
-local timer = 0, checker = false
-if not checker then
-  timer = os.time()
-  checker = true
-end
 
-local node = minetest.get_node_or_nil({ x = 1, y = 3, z = 4 })
+-- ROT PURGATOR
+minetest.register_node("rotmod:sacred_rot", {
+description = "rot",
+tiles = {"rot_sacred.png"},
+groups = {cracky=3}
+})
 
-(dump(node)) --> { name=.., param1=.., param2=.. }
---local node_pos   = minetest.find_node_near(pos, 1, {"rotMode:dirt_rot"})
-if os.difftime(timer, os.time())>=1 then
-  timer = os.time()
-  local node_pos = minetest.find_node_near(pos, 1, {"default:grass"})
-  if node_pos then
-  local grassNode1 = minetest.get_node_or_nil({ x = 1, y = 3, z = 4 })
-  minetest.swap_node({ x = 1, y = 3, z = 4 }, { name = "rotMode:dirt_rot"})
-end
+minetest.register_abm({
+    nodenames = {"default:dirt_with_grass"},
+    interval = 10.0,
+    chance = 50,
+    action = function(pos, node, active_object_count,
+            active_object_count_wider)
+        local targetPos = {x = pos.x, y = pos.y, z = pos.z}
+        minetest.set_node(targetPos, {name = "rotmod:dirt_rot"})
+    end
+})
 
-end
+minetest.register_abm({
+    nodenames = {"default:dirt_with_grass"},
+    neighbors = {"default:dirt_with_grass", "rotmod:dirt_rot"},
+    interval = 1.0,
+    action = function(pos, node, active_object_count,
+            active_object_count_wider)
+        local targetPos = {x = pos.x, y = pos.y, z = pos.z}
+        minetest.set_node(targetPos, {name = "rotmod:dirt_rot"})
+    end
+})
+
+minetest.register_abm({
+    nodenames = {"rotmod:dirt_rot"},
+    interval = 3.0,
+    action = function(pos, node, active_object_count,
+            active_object_count_wider)
+        local targetPos = {x = pos.x, y = pos.y, z = pos.z}
+        minetest.set_node(targetPos, {name = "air"})
+    end
+})
+
