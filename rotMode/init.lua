5ed42cfe05afda5d8ccb5853aee7007229af38e3
Pablo <pablo.scagliotti@gmail.com>
2020-11-26 18:31:20 +0100

    rotModd 0.3

--- rotMode/init.lua
+++ rotMode/init.lua
@@ -1,7 +1,13 @@
 -- ROT STONE
 minetest.register_node("rotMode:dirt_rot", {
 description = "rot",
-tiles = {"dirt_rot.png"},
+tiles = {"rot_up.png",    -- y+
+        "rot_down.png",  -- y-
+        "rot_side.png", -- x+
+        "rot_side.png",  -- x-
+        "rot_side.png",  -- z+
+        "rot_side.png",}, --z-
 groups = {snappy = 1, bendy = 2, cracky = 1, melty = 2, level = 2},
 sounds = default_metal_sounds,
+
 })
