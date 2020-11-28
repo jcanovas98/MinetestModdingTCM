884895e529cfaef9ca0a1a1f30fb3e1388f528cd
Pablo <pablo.scagliotti@gmail.com>
2020-11-28 18:55:27 +0100

    teleport with image

--- Teleport/init.lua
+++ Teleport/init.lua
@@ -1,13 +1,9 @@
 -- teleport cube
 minetest.register_node("teleport:teleport", {
 description = "teleport",
-tiles = {"rot_up.png",   -- y+
-        "rot_down.png",  -- y-
-        "rot_side.png",  -- x+
-        "rot_side.png",  -- x-
-        "rot_side.png",  -- z+
-        "rot_side.png",  -- z-
-      }, 
-is_ground_content = false,
-groups = {crumbly=3}
+tiles = {"teleport_node",  -- z-
+      },
+groups = {cracky=3}
 })
+
+minetest.
