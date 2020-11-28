0e55da77f207ea0e1c0c43cbd916eb9403156474
jcanovas <jcanovas@edu.tecnocampus.cat>
2020-11-28 21:02:38 +0100

    teleport v1.noseke falta k haga el command de tp

--- Teleport/init.lua
+++ Teleport/init.lua
@@ -1,6 +1,6 @@
 -- teleport cube
 minetest.register_node("teleport:teleport_pad", {
-description = "teleport",
+description = "Teleport",
 tiles = {"teleport_up.png",   -- y+
         "teleport_down.png",  -- y-
         "teleport_side.png",  -- x+
@@ -8,12 +8,26 @@ tiles = {"teleport_up.png",   -- y+
         "teleport_side.png",  -- z+
         "teleport_side.png",  -- z-
       }, 
-groups = {cracky=3}
+groups = {cracky=3},
+on_construct = function(pos)
+  local player = minetest.get_player_by_name("singleplayer")
+  local inv = player:get_inventory()
+  local stack = ItemStack("teleport:teleport_command")
+  local meta = stack:get_meta()
+  meta:set_int("x", pos.x)
+  meta:set_int("y", pos.y + 1)
+  meta:set_int("z", pos.z)
+  local leftover = inv:add_item("main", stack)
+end
 })
 
 -- teleport command
-
 minetest.register_craftitem("teleport:teleport_command", {
     description = "Command",
-    inventory_image = "teleport_controller.png"
+    inventory_image = "teleport_controller.png",
+    on_use = function(itemstack, user, pointed_thing)
+        local meta = itemstack:get_meta()
+        local pos = {x=meta:get_int("x"), y=meta:get_int("y"), z=meta:get_int("z")
+        
+      end
 })
