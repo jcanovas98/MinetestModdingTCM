afc05b40ffc3d92a2cd0839addd8eb342ae347d1
jcanovas <jcanovas@edu.tecnocampus.cat>
2020-11-28 20:27:09 +0100

    rotmod v2 final

--- rotMode/init.lua
+++ rotMode/init.lua
@@ -1,6 +1,6 @@
 -- ROT STONE
 minetest.register_node("rotmod:dirt_rot", {
-description = "rot",
+description = "Rotten Dirt",
 tiles = {"rot_up.png",   -- y+
         "rot_down.png",  -- y-
         "rot_side.png",  -- x+
@@ -14,7 +14,7 @@ groups = {crumbly=3}
 
 -- ROT PURGATOR
 minetest.register_node("rotmod:sacred_rot", {
-description = "rot",
+description = "Sacred Purgator",
 tiles = {"rot_sacred.png"},
 groups = {cracky=3}
 })
@@ -22,7 +22,8 @@ groups = {cracky=3}
 minetest.register_abm({
     nodenames = {"default:dirt_with_grass"},
     interval = 10.0,
-    chance = 50,
+    chance = 200,
+    catch_up = false,
     action = function(pos, node, active_object_count,
             active_object_count_wider)
         local targetPos = {x = pos.x, y = pos.y, z = pos.z}
@@ -32,8 +33,9 @@ minetest.register_abm({
 
 minetest.register_abm({
     nodenames = {"default:dirt_with_grass"},
-    neighbors = {"default:dirt_with_grass", "rotmod:dirt_rot"},
-    interval = 1.0,
+    neighbors = {"rotmod:dirt_rot"},
+    interval = 2.0,
+    catch_up = false,
     action = function(pos, node, active_object_count,
             active_object_count_wider)
         local targetPos = {x = pos.x, y = pos.y, z = pos.z}
@@ -43,7 +45,8 @@ minetest.register_abm({
 
 minetest.register_abm({
     nodenames = {"rotmod:dirt_rot"},
-    interval = 3.0,
+    interval = 8.0,
+    catch_up = false,
     action = function(pos, node, active_object_count,
             active_object_count_wider)
         local targetPos = {x = pos.x, y = pos.y, z = pos.z}
@@ -51,11 +54,32 @@ minetest.register_abm({
     end
 })
 
---[[Cogiendo como referencia el codigo de ejemplo visto antes, cargaremos el content id de los
-nodos default:dirt y rot:dirt, crearemos una función purge_rotdirt(pos, size) donde le
-pasaremos una posición y en un cubo de tamaño size con centro pos convertiremos todos los
-nodos de rot:dirt en default:dirt.
-Por ultimo solo nos faltaria crear un ABM para el nodo rot:purgator para llamar a nuestra
-función purge_rotdirt y asi eliminar los nodos de tierra enferma.--]]
+local rot = minetest.get_content_id("rotmod:dirt_rot")
+local grass = minetest.get_content_id("default:dirt_with_grass")
 
+minetest.register_abm({
+    nodenames = {"rotmod:sacred_rot"},
+    interval = 0.01,
+    action = function(pos)
+      local pos1 = {x = pos.x - 10, y = pos.y - 10, z = pos.z - 10}
+      local pos2 = {x = pos.x + 10, y = pos.y + 10, z = pos.z + 10}
+      local vm = minetest.get_voxel_manip()
+      local emin, emax = vm:read_from_map(pos1, pos2)
+      local a = VoxelArea:new{ MinEdge = emin, MaxEdge = emax }
+      local data = vm:get_data()
+      for z = pos1.z, pos2.z do
+        for y = pos1.y, pos2.y do
+          for x = pos1.x, pos2.x do
+            local vi = a:index(x, y, z)
+            if data[vi] == rot then
+              data[vi] = grass
+            end
+          end
+        end
+      end
+    vm:set_data(data)
+    vm:write_to_map(true)
+    end
+      
+})
 
